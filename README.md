444b422439ab19fb1bc0bb9c6c47879a56a420c2
aouwalitshikkha <aouwalitshikkha@gmail.com>
2022-10-13 18:21:56 +0600

    initial Commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# Python Couse Assignment One 
+Welcome to Python Course Assignment One. As you have completed the milestone one, that means you are familier with the python basic. This assignment helps you to grow your confidennce 
+
+## What you need to do? 
+
+1. *Step-1*: You need to clone this repository first. You can clone the repository by following command
+
+```code
+git clone https://github.com/aouwalitshikkha/assignments-one.git
+```
+
+2. Open it in your favorite IDE e.g. PyCharm, VS Code etc.
+3. Here you will get three files
+
+### Task One 
+1. Open <code>mobile.py</code>.
+2. You will get a varibale named <code>mobile_data</code>. You don't need to edit it.
+3. At the end of the page you will get a comment where you can start code 
+4. Make a template using the dictionary data.  
+5. Your Template must have at least two sentences. 
+6. USD must be converted to BDT
+6. example Output: 
+
+```code
+Xiaomi Note 5 is made in China. The price is 300 USD which is almost equal to 30975 BDT
+
+```
+
+# Task Two
+1. Open <code>post data.py</code>.
+2. You will get a list of dictionaries. You don't need to edit it.
+3. At the end of the page you will get a comment where you can start code 
+4. Add slug field to all the post 
+5. We will test it by calling with random number e.g post_data[4]
+6. Expected Output: 
+
+```code
+{
+    'userId': 'Alex Gates',
+     'id': 5, 
+     'title': ' nesciunt quas odio', 
+     'body': 'repudiandae veniam quaerat sunt sed.....', 
+     'slug': '-nesciunt-quas-odio'
+}
+```
+## For bonus
+1. make the slug limited to 3 words
+2. Make variation of the templates of mobile data
+
+## Take Care of 
+1. Variable name must be meaningfull
+2. Commit at least 2 times in github
+
---
+++ post_data.py
@@ -0,0 +1,46 @@
+post_data = [
+    {
+        "userId":  "Alex Gates",
+        "id": 1,
+        "title": "sunt aut facere repellat provident",
+        "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+    },
+    {
+        "userId":  "Alex Gates",
+        "id": 2,
+        "title": "qui est esse ",
+        "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+    },
+    {
+        "userId":  "Alex Gates",
+        "id": 3,
+        "title": "ea molestias quasi exercitationem ",
+        "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
+    },
+    {
+        "userId":  "Alex Gates",
+        "id": 4,
+        "title": "eum et est occaecati ",
+        "body": "ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit"
+    },
+    {
+        "userId":  "Alex Gates",
+        "id": 5,
+        "title": " nesciunt quas odio",
+        "body": "repudiandae veniam quaerat sunt sed\nalias aut fugiat sit autem sed est\nvoluptatem omnis possimus esse voluptatibus quis\nest aut tenetur dolor neque"
+    },
+    {
+        "userId":  "Alex Gates",
+        "id": 6,
+        "title": "dolorem eum magni eos aperiam quia ",
+        "body": "ut aspernatur corporis harum nihil quis provident sequi\nmollitia nobis aliquid molestiae\nperspiciatis et ea nemo ab reprehenderit accusantium quas\nvoluptate dolores velit et doloremque molestiae"
+    }, ]
+
+# Your Code Start from here
+
+
+
+
+# Your code ends here
+
+print(post_data[4])

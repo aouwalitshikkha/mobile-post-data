43414412d3b64aa9bb52e5ba63be5f655c5dccc3
aouwalitshikkha <aouwalitshikkha@gmail.com>
2022-10-17 19:54:36 +0600

    Added Comma

--- mobile.py
+++ mobile.py
@@ -1,12 +1,12 @@
 mobile_data = {
     'status': True,
     'data': [
-        {'name': 'Xiaomi Note 5', 'price': '300 USD', 'made': 'China'}
-        {'name': 'Samsung Note 6', 'price': '200 USD', 'made': 'USA'}
-        {'name': 'Iphone 5', 'price': '180.5 USD', 'made': 'Japan'}
-        {'name': 'Pixel 5', 'price': '89.60 USD', 'made': 'Rusia'}
-        {'name': 'Techno 5', 'price': '110 USD', 'made': 'Uk'}
-        {'name': 'Huawei 5', 'price': '350 USD', 'made': 'Malaysia'}
+        {'name': 'Xiaomi Note 5', 'price': '300 USD', 'made': 'China'},
+        {'name': 'Samsung Note 6', 'price': '200 USD', 'made': 'USA'},
+        {'name': 'Iphone 5', 'price': '180.5 USD', 'made': 'Japan'},
+        {'name': 'Pixel 5', 'price': '89.60 USD', 'made': 'Rusia'},
+        {'name': 'Techno 5', 'price': '110 USD', 'made': 'Uk'},
+        {'name': 'Huawei 5', 'price': '350 USD', 'made': 'Malaysia'},
     ],
     'exchnage_rate': 103.25
 }
